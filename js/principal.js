328e47d5dd67d0359c4ab5e8685027dfe152af85
Pedro Azevedo <theazevedo@outlook.com>
2017-09-16 15:33:24 -0300

    update

--- js/principal.js
+++ js/principal.js
@@ -1,1 +1,15 @@
-var elemento = document.querySelector("#troca-display");
+var botao = document.querySelector("#mudaLayout").addEventListener("click", function(){
+    var mural = document.querySelector(".mural");
+    mural.classList.toggle("mural--linhas");
+    if (mural.classList.contains("mural--linhas")){
+        this.textContent = "Blocos";
+    }else{
+        this.textContent = "Linhas";
+    }
+})
+
+
+// function mudaLayout(){
+//     document.querySelector(".mural").classList.toggle("mural--linhas");
+
+// }
