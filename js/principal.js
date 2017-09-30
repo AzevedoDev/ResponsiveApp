c5fe1f6cf09f88e8f5910b588baa1ae7291a3093
Pedro Azevedo <theazevedo@outlook.com>
2017-09-30 13:14:22 -0300

    auia 3

--- js/principal.js
+++ js/principal.js
@@ -8,14 +8,47 @@ var botao = document.querySelector("#mudaLayout").addEventListener("click", func
     }
 })
 
+function adicionaCartao(conteudo,cor){
+    
+    contador++;
+    
+
+        var botaoRemove = $("<button>").addClass("opcoesDoCartao-remove")
+                                        .addClass("opcoesDoCartao-opcao")
+                                        .attr("data-ref" , contador)
+                                        .text("remover")
+                                        .click(removeCartao);
+
+
+        var opcoes = $("<div>").addClass("opcoesDoCartao")
+                    .append(botaoRemove);
+
+        var conteudoTag = $("<p>").addClass("cartao-conteudo")
+                    .append(conteudo);
+
+        
+        var tipoCartao = decideTipoCartao(conteudo);
+
+        
+        $("<div>").attr("id","cartao_" + contador)
+                    .addClass("cartao")
+                    .addClass(tipoCartao)
+                    .append(opcoes)
+                    .append(conteudoTag)
+                    .css("background-color",cor)
+                    .prependTo(".mural");
+};
+
 function removeCartao(){
     var cartao = document.querySelector("#cartao_" + this.dataset.ref);
     cartao.classList.add("cartao--some");
     setTimeout(function(){
         cartao.remove();
     },400);
-}
+};
+
 var botoes = document.querySelectorAll(".opcoesDoCartao-remove");
+
 for (var i=0;i<botoes.length;i++){
     botoes[i].addEventListener("click", removeCartao);
 };
@@ -30,32 +63,18 @@ $(".novoCartao").submit(function(event){
     event.preventDefault();
 
     var campoConteudo = $(".novoCartao-conteudo");
-    var conteudo = campoConteudo.val().trim().replace(/\n/g, "<br>");
+    var conteudo = campoConteudo.val()
+                                .trim()
+                                .replace(/\*\*(.+?)\*\*/g, "<strong> $1 </strong")
+                                .replace(/\*(.+?)\*/g, "<em> $1 </em")
+                                .replace(/\n/g, "<br>");
 
 
     if(conteudo){
         
-        contador++;
-
-    
-
-        var botaoRemove = $("<button>").addClass("opcoesDoCartao-remove")
-                                       .addClass("opcoesDoCartao-opcao").attr("data-ref" , contador).text("remover").click(removeCartao);
-
-        var opcoes = $("<div>").addClass("opcoesDoCartao").append(botaoRemove);
-
-        var conteudoTag = $("<p>").addClass("cartao-conteudo").append(conteudo);  
-        
-       var tipoCartao = decideTipoCartao(conteudo);
-       
-        $("<div>").attr("id","cartao_" + contador)
-                    .addClass("cartao")
-                    .addClass(tipoCartao)
-                    .append(opcoes)
-                    .append(conteudoTag)
-                    .prependTo(".mural");
+        adicionaCartao(conteudo, "#EBEF40");
                     
-    }
+    };
 
     campoConteudo.val("");
 });
@@ -100,5 +119,71 @@ $("#busca").on("input", function(){
         }
     });
 
+    $("#ajuda").click(function(){
+        $.getJSON("https://ceep.herokuapp.com/cartoes/instrucoes",
+            function(res){
+                console.log(res);
+       
+                res.instrucoes.forEach(function(instrucao){
+                    adicionaCartao(instrucao.conteudo, instrucao.cor);
+                });
+            }
+        );
+    });
 
-    https://tableless.com.br/o-basico-sobre-expressoes-regulares/
+$("#sync").click(function(){
+
+    var cartoes = [];
+
+    $("#sync").removeClass("botaoSync--sincronizado");
+    $("#sync").addClass("botaoSync--esperando");
+
+    $(".cartao").each(function(){
+        var cartao={};
+        cartao.conteudo = $(this).find(".cartao-conteudo").html();
+        cartoes.push(cartao);
+    });
+
+    var mural = {
+        usuario: usuario
+        ,cartoes: cartoes
+    }
+
+    $.ajax({
+        url: "https://ceep.herokuapp.com/cartoes/salvar"
+        ,method: "POST"
+        ,data: mural
+        ,success: function(res){
+            $("#sync").addClass("botaoSync--sincronizado");
+            console.log(res.quantidade + " cartões salvos em "
+        + res.usuario);
+        }
+        ,error: function(){
+            $("#sync").addClass("botaoSync--deuRuim");
+            console.log("Não foi possivel salvar o mural");
+        }
+        ,complete: function(){
+            $("#sync").removeClass("botaoSync--esperando");
+            
+        }
+    });
+});
+
+var usuario = "seu.email@exemplo.com.br";
+
+
+$.getJSON(
+    "https://ceep.herokuapp.com/cartoes/carregar?callback=?",
+    {usuario: usuario},
+    function(res){
+        var cartoes = res.cartoes;
+        console.log(cartoes.length + " carregados em " + res.usuario);
+        cartoes.forEach(function(cartao){
+            adicionaCartao(cartao.conteudo);
+        });
+    }
+);
+
+
+
+    // https://tableless.com.br/o-basico-sobre-expressoes-regulares/
